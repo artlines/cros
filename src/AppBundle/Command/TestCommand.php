0534eede67560f29ef9888829cdc24d3189c690a
alf1kknag <e.nachuychenko@nag.ru>
2018-04-18 12:17:42 +0500

    add command to test

---
+++ src/AppBundle/Command/TestCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Service\Sms;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('app:test:any');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @throws \Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getContainer()->get('doctrine');
+        $orgRepo = $em->getRepository('AppBundle:Organization');
+
+        $orgs = $orgRepo->findByIdsOrganizationApproved();
+
+        var_dump($orgs);
+    }
+}
