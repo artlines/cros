aec3b6df3354eccf70e0196ae9a98e2542e1269d
Stas Borzih <s.borzih@nag.ru>
2018-04-23 15:38:46 +0500

    fixed bug resize images alpha

--- src/AppBundle/Service/ResizeImages.php
+++ src/AppBundle/Service/ResizeImages.php
@@ -61,6 +61,8 @@ class ResizeImages {
     }
     function resize($width,$height) {
         $new_image = imagecreatetruecolor($width, $height);
+        imagealphablending($new_image, false);
+        imagesavealpha($new_image, true);
         imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
         $this->image = $new_image;
     }
