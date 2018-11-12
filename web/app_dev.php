a7d15f98fad542b3afe35f5b8f55b96242f94b8a
alf1kknag <e.nachuychenko@nag.ru>
2018-11-12 11:23:15 +0500

    edit access IPs

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,7 +10,7 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (!(in_array(@$_SERVER['REMOTE_ADDR'], array('192.168.5.22', '195.211.31.245', '176.215.13.125', '172.31.74.224', '172.31.74.109', '127.0.0.1', '::1', '94.230.139.2', '87.251.176.34', '82.112.32.85', '5.189.43.55', '192.168.5.82', '5.135.190.34', '192.168.5.212','172.31.74.101', '195.211.31.240')))
+if (!(in_array(@$_SERVER['REMOTE_ADDR'], array('192.168.5.22', '192.168.5.183', '195.211.31.245', '127.0.0.1', '::1')))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information. '.$_SERVER['REMOTE_ADDR']);
