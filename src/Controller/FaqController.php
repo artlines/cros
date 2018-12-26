890765fa9b149cda3e93e8dc33f894cc367423c9
alf1kknag <e.nachuychenko@nag.ru>
2018-12-26 09:27:15 +0500

    refactor

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -2,57 +2,27 @@
 
 namespace App\Controller;
 
-use AppBundle\Entity\Apartament;
-use AppBundle\Entity\ApartamentId;
-use AppBundle\Entity\Conference;
-use AppBundle\Entity\Info;
-use AppBundle\Entity\Logs;
-use AppBundle\Entity\Organization;
-use AppBundle\Entity\Organizations;
-use AppBundle\Entity\OrgToConf;
-use AppBundle\Entity\User;
-use AppBundle\Entity\UserToApartament;
-use AppBundle\Entity\UserToConf;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Old\Entity\AppendText;
+use App\Old\Entity\Faq;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class FaqController extends AbstractController
 {
-
-    /**
-     * client
-     */
-    private $client = null;
-
-    public function setUp(){
-        $this->client = static::createClient();
-    }
-
     /**
      * @Route("/faq", name="faq")
      */
     public function faq()
     {
         /** @var Faq $faq */
-        $faq = $this->getDoctrine()
-            ->getRepository('App:Faq')
-            ->findBy(array('isActive' => true));
+        $faq = $this->getDoctrine()->getRepository('App:Faq')->findBy(['isActive' => true]);
 
         /** @var AppendText $append_text */
-        $append_text = $this->getDoctrine()
-            ->getRepository('App:AppendText')
-            ->findOneBy(array('alias' => 'faq'));
+        $append_text = $this->getDoctrine()->getRepository('App:AppendText')->findOneBy(['alias' => 'faq']);
 
-        return $this->render('frontend/faq/show.html.twig', array(
-
-            'faq' => $faq,
-            'append_text' => $append_text,
-        ));
+        return $this->render('frontend/faq/show.html.twig', [
+            'faq'           => $faq,
+            'append_text'   => $append_text,
+        ]);
     }
 }
---
+++ src/Controller/Site/ArchiveController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alf1kk
+ * Date: 26.12.18
+ * Time: 8:49
+ */
+
+namespace App\Controller\Site;
+
+
+class ArchiveController
+{
+
+}
