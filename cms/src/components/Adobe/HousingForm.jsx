1f221ba1edf97ffb2deddd0056a134439d443e5f
alf1kknag <e.nachuychenko@nag.ru>
2019-01-10 13:58:50 +0500

    Housing form start

---
+++ cms/src/components/Adobe/HousingForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class HousingForm extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+
+
+        return (
+
+        );
+    }
+}
