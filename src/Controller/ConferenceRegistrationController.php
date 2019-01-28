27754c1870379cfed3c91ad76d6142b8219522bb
Evgeniy Syuzev <e.suzev@nag.ru>
2019-01-28 15:12:43 +0500

    main ConferenceRegistrationFormType

--- src/Controller/ConferenceRegistrationController.php
+++ src/Controller/ConferenceRegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ConferenceRegistrationFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +12,16 @@ class ConferenceRegistrationController extends AbstractController
     /**
      * @Route("/conference/registration", name="conference_registration")
      */
-    public function index()
+    public function index(Request $request)
     {
 //        $form = $this->createForm(MyFrmType::class,null,['attr'=>['class'=>'row']]);
         $form = $this->createForm(ConferenceRegistrationFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //dd($form->getData());
+        }
 
         return $this->render('conference_registration/index.html.twig', [
 //            'controller_name' => 'ConferenceRegistrationController',
--- src/Form/ConferenceRegistrationFormType.php
+++ src/Form/ConferenceRegistrationFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Participating\Organization;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,47 +14,133 @@ class ConferenceRegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+/*
+При входе на страницу регистрации показывать форму с полями:
+
++ Наименование организации (обязательное)
+Логотип компании - загрузка файла - png
++ Город (обязательное)
++ Адрес (обязательное)
++ ИНН (обязательное)
++ КПП (обязательное)
++ Реквизиты (с подстановкой текста -
+  Полное наименование организации,
+  ОГРН, Юридический адрес, Почтовый адрес, Банк, БИК, К/С, Р/С) (обязательное)
+Комментарий
+*/
+
         $builder
+//            ->add('dueDate', null, ['widget' => 'single_text'])
             ->add(
                 'name',
                 TextType::class,
                 array(
-//                    'label' => 'Название организации',
+                    'label' => 'Organization.Name.Label',
                     'attr' => array(
-                        'class' => 'col-md-6',
-                        'placeholder' => 'Ёлки-телеком',
-                        'data-helper' => 'Ваш основной Торговый знак, будет использоваться на бейджах и визитках'
+                        'class' => 'col-md-6 cs-theme-color-gray-dark-v3',
+                        'placeholder' => 'Organization.Name.PlaceHolder',
                     ),
-                    'help' => '.help',
-                    'required' => true
+                    'help' => 'Organization.Name.Help',
+                    'required' => true,
                 )
             )
-            ->add('city',
+            ->add(
+                'city',
                 TextType::class,
                 array(
-                    'label' => 'City',
+                    'label' => 'Organization.City.Label',
                     'attr' => array(
-                        'class' => 'cs-theme-color-gray-dark-v3 col-md-6'
-                    )
+                        'class' => 'col-md-6 cs-theme-color-gray-dark-v3',
+                        'placeholder' => 'Organization.City.PlaceHolder',
+                    ),
+                    'help' => 'Organization.City.Help',
+                    'required' => true,
                 )
             )
+            ->add(
+                'address',
+                TextType::class,
+                array(
+                    'label' => 'Organization.Address.Label',
+                    'attr' => array(
+                        'class' => 'col-md-6 cs-theme-color-gray-dark-v3',
+                        'placeholder' => 'Organization.Address.PlaceHolder',
+                    ),
+                    'help' => 'Organization.Address.Help',
+                    'required' => true,
+                )
+            )
+            ->add(
+                'inn',
+                TextType::class,
+                array(
+                    'label' => 'Organization.Inn.Label',
+                    'attr' => array(
+                        'class' => 'col-md-3 cs-theme-color-gray-dark-v3',
+                        'placeholder' => 'Organization.Inn.PlaceHolder',
+                    ),
+                    'help' => 'Organization.Inn.Help',
+                    'required' => true,
+                )
+            )
+            ->add(
+                'kpp',
+                TextType::class,
+                array(
+                    'label' => 'Organization.Kpp.Label',
+                    'attr' => array(
+                        'class' => 'col-md-3 cs-theme-color-gray-dark-v3',
+                        'placeholder' => 'Organization.Kpp.PlaceHolder',
+                    ),
+                    'help' => 'Organization.Kpp.Help',
+                    'required' => true,
+                )
+            )
+
 //            ->add('name')
-            ->add('email')
 //            ->add('city')
-            ->add('requisites')
-            ->add('address')
-            ->add('isActive')
-            ->add('inn')
-            ->add('kpp')
-            ->add('hidden')
-            ->add('comment')
-            ->add('country')
-            ->add('typePerson')
-            ->add('createdAt')
+            ->add(
+                'requisites',
+                TextareaType::class,
+                array(
+                    'label' => 'Organization.Requisites.Label',
+                    'attr' => array(
+                        'class' => 'col-md-6 cs-theme-color-gray-dark-v3',
+                        'placeholder' => 'Organization.Requisites.PlaceHolder',
+                        'rows' => '8',
+                    ),
+                    'data' => "Полное наименование организации: \nОГРН: \nЮридический адрес: \nПочтовый адрес: \nБанк: \nБИК: \nК/С: \nР/С:",
+                    'help' => 'Organization.Requisites.Help',
+                    'required' => true,
+                )
+
+            )
+            ->add(
+                'comment',
+                TextareaType::class,
+                array(
+                    'label' => 'Organization.Comment.Label',
+                    'attr' => array(
+                        'class' => 'col-md-6 cs-theme-color-gray-dark-v3',
+                        'placeholder' => 'Organization.Comment.PlaceHolder',
+                        'rows' => '4',
+                    ),
+                    'help' => 'Organization.Comment.Help',
+                    'required' => false,
+                )
+            )
+
+            ->add('save', SubmitType::class)
+
+//            ->add('isActive')
+//            ->add('hidden')
+//            ->add('country')
+//            ->add('typePerson')
+//            ->add('email')
+//            ->add('createdAt')
             // ['attr'=>['class'=>'row']]
 //            ->setAttribute('id','row')
         ;
-        $options['attr']['class'] = 'row';
 
 
     }
@@ -61,7 +149,7 @@ class ConferenceRegistrationFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Organization::class,
-            'attr' => ['class'=>'row'],
+//            'attr' => ['class'=>'row'],
         ]);
     }
 }
