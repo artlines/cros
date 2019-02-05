9950363f2d75633e58862fadd8a5a3bc155280d1
alf1kknag <e.nachuychenko@nag.ru>
2019-02-05 09:30:47 +0500

    add tread to invite email

--- src/Controller/Api/V1/ConferenceOrganizationController.php
+++ src/Controller/Api/V1/ConferenceOrganizationController.php
@@ -311,7 +311,7 @@ class ConferenceOrganizationController extends ApiController
 
         $data = $inviteData;
         $data['hash'] = $inviteHash;
-        $mailer->send('TEST', $data, $email, null, ['e.nachuychenko@nag.ru', 'cros@nag.ru']);
+        $mailer->send('Приглашаем вас на КРОС-2019', $data, $email, null, ['e.nachuychenko@nag.ru', 'cros@nag.ru']);
 
         return $this->success();
     }
@@ -347,7 +347,7 @@ class ConferenceOrganizationController extends ApiController
             return $this->badRequest('Параметры для приглашения не заполнены.');
         }
         $data['hash'] = $conferenceOrganization->getInviteHash();
-        $mailer->send('TEST', $data, $email, null, ['e.nachuychenko@nag.ru', 'cros@nag.ru']);
+        $mailer->send('Приглашаем вас на КРОС-2019', $data, $email, null, ['e.nachuychenko@nag.ru', 'cros@nag.ru']);
 
         return $this->success();
     }
