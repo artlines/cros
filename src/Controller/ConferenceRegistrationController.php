1b7e197620a044a908e5041a6e44b7de18903aca
Evgeniy Syuzev <e.suzev@nag.ru>
2019-02-08 08:14:25 +0500

    store ConferenceOrganization.notes to content

--- src/Controller/ConferenceRegistrationController.php
+++ src/Controller/ConferenceRegistrationController.php
@@ -359,15 +359,6 @@ class ConferenceRegistrationController extends AbstractController
                     $comment->setUser($conferenceMember->getUser());
                     // NOT DO $comment->setContent($ConferenceOrganization->getNotes());
                     $content = $request->get('conference_organization_form')['notes'];
-                    if ($content) {
-                        $comment->setContent($content);
-                    }
-                    $em->persist($comment);
-                    $comment = new Comment();
-                    $comment->setConferenceOrganization($ConferenceOrganization);
-                    $comment->setUser($conferenceMember->getUser());
-                    // NOT DO $comment->setContent($ConferenceOrganization->getNotes());
-                    $content = $request->request->get('conference_organization_form')['notes'];
                     $comment->setContent($content);
                     if ($content) {
                         $em->persist($comment);
