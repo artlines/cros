e64bfaf897efe02ac8cc51ff57424bff6cd90ec4
alf1kknag <e.nachuychenko@nag.ru>
2019-02-08 12:12:36 +0500

    add sort to dictionaries at settings page && add summary information

--- src/Repository/Abode/RoomTypeRepository.php
+++ src/Repository/Abode/RoomTypeRepository.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: esuzev
- * Date: 04.02.19
- * Time: 11:10
- */
 
 namespace App\Repository\Abode;
-
 
 use App\Entity\Abode\Room;
 use App\Entity\Conference;
@@ -16,8 +9,8 @@ use Doctrine\ORM\EntityRepository;
 
 class RoomTypeRepository  extends EntityRepository
 {
-
-    function findAllFreeForConference($conference_id){
+    public function findAllFreeForConference($conference_id)
+    {
         return $this
             ->createQueryBuilder('rt')
             ->leftJoin(
@@ -43,4 +36,29 @@ class RoomTypeRepository  extends EntityRepository
             ->getResult();
     }
 
+    /**
+     * Return information about settlement by room types
+     */
+    public function getSummaryInformation()
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $stmt = $conn->prepare("
+            SELECT
+              rt.id AS room_type_id,
+              COUNT(DISTINCT cm.id) AS busy,
+              COUNT(DISTINCT p.id) AS populated,
+              COUNT(DISTINCT r.id)*rt.max_places AS total
+            FROM abode.room_type rt
+              LEFT JOIN abode.room r ON rt.id = r.type_id
+              LEFT JOIN participating.conference_member cm ON rt.id = cm.room_type_id
+              LEFT JOIN abode.place p ON cm.id = p.conference_member_id
+            GROUP BY rt.id
+        ");
+
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        return $result;
+    }
 }
