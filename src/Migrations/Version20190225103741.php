3fc1d4edbcaf01b4821a15edd845f303739adfef
alf1kknag <e.nachuychenko@nag.ru>
2019-02-26 11:36:02 +0500

    add migration to new fields

---
+++ src/Migrations/Version20190225103741.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190225103741 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE participating.member ADD b2b_guid VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_48AAA8AA3D442A31 ON participating.member (b2b_guid)');
+        $this->addSql('ALTER TABLE participating.invoice ADD b2b_order_guid VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE participating.invoice ADD fixed_amount NUMERIC(12, 2) DEFAULT NULL');
+        $this->addSql('ALTER TABLE participating.invoice ADD bill_sent BOOLEAN DEFAULT \'false\' NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_F880EAEE71162C81 ON participating.invoice (b2b_order_guid)');
+        $this->addSql('ALTER TABLE participating.organization ADD b2b_guid VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D78D9D53D442A31 ON participating.organization (b2b_guid)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+    }
+}
