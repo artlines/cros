333808f67854c6b3bb84d1c9377c50c8f6c8482f
alf1kknag <e.nachuychenko@nag.ru>
2019-03-13 14:13:53 +0500

    fix

--- cms/src/pages/Program.jsx
+++ cms/src/pages/Program.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import {
+    AppBar,
+    Tab,
+    Tabs,
+} from '@material-ui/core';
 
-class Program extends React.Component {
-    constructor(props) {
-        super(props);
+function Program() {
+    const [value, setValue] = React.useState(0);
+
+    function handleChange(event, newValue) {
+        setValue(newValue);
     }
 
-    render() {
-
-        return (
-            <React.Fragment>
-                asd
-            </React.Fragment>
-        );
-    }
+    return (
+        <AppBar position={`static`}>
+            <Tabs value={value} variant={`fullWidth`} onChange={handleChange}>
+                <Tab label={`Комитет`}/>
+                <Tab label={`Спикеры`}/>
+                <Tab label={`Расписание`}/>
+            </Tabs>
+        </AppBar>
+    );
 }
 
 export default Program;
