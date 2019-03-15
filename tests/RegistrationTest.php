83ae42fbf91c53bab00ffda40dc246cdc4ba764d
Evgeniy Syuzev <e.suzev@nag.ru>
2019-03-15 15:40:30 +0500

    Тестирование Дубликата по компании

--- tests/RegistrationTest.php
+++ tests/RegistrationTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Entity\Participating\ConferenceOrganization;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RegistrationTest extends WebTestCase
@@ -51,6 +52,7 @@ class RegistrationTest extends WebTestCase
         );
 
 //        dd($client->getResponse());
+        $this->setName('dfdf');
         $this->assertSame(200, $client->getResponse()->getStatusCode());
         $json = json_decode($client->getResponse()->getContent(),true);
 //        dd($client->getResponse()->getContent());
@@ -71,12 +73,12 @@ class RegistrationTest extends WebTestCase
         $crawler = $client->request(
             'POST',
             '/test/registration',
-            array (
+            array(
                 'conference_organization_form' =>
-                    array (
+                    array(
                         'conference' => '272',
                         'organization' =>
-                            array (
+                            array(
                                 'name' => 'fghfghgh',
                                 'city' => 'fghfhfh',
                                 'address' => 'xcvxcvxcv',
@@ -94,6 +96,80 @@ class RegistrationTest extends WebTestCase
                                     'sex' => '1',
                                     'phone' => '8(922)209-24-69',
                                     'email' => 'esuzev+2@test.com',
+                                    'post' => 'sdadasd',
+                                    'representative' => '1',
+                                ],
+                                'arrival' => '23.05.2019 10:00',
+                                'leaving' => '24.05.2019 12:00',
+                                'carNumber' => '',
+                                'RoomType' => '2',
+                                'neighbourhood' => '',
+                            ],
+                        ],
+                        'save' => '',
+                    ),
+            )
+        );
+
+//        dd($client->getResponse());
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $error_json = json_decode($client->getResponse()->getContent(),true);
+//        dd($client->getResponse()->getContent());
+        $this->assertSame(['errors'=>['email' => 'Пользователь с такой почтой уже зарегистрирован']],$error_json);
+
+//        $this->assertContains('Ваша заявка принята',
+//            $crawler
+//                ->filter('div.container p')
+//                ->text());
+    }
+
+    public function testFindOrgByINN()
+    {
+        $kernel = self::bootKernel();
+        $entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        /** @var ConferenceOrganization $testConferenceOrganization */
+        $testConferenceOrganization = $entityManager
+            ->getRepository(ConferenceOrganization::class)
+            ->findOneById(473);
+        ;
+
+        $this->assertEquals(473, $testConferenceOrganization->getId() );
+
+        $testConferenceOrganization->setFinish(false);
+        $entityManager->persist($testConferenceOrganization);
+        $entityManager->flush();
+        //if (!);
+
+        $client = static::createClient();
+        $crawler = $client->request(
+            'POST',
+            '/test/registration',
+            array (
+                'conference_organization_form' =>
+                    array (
+                        'conference' => $testConferenceOrganization->getConference()->getId(),
+                        'organization' =>
+                            array (
+                                'name' => 'fghfghgh',
+                                'city' => 'fghfhfh',
+                                'address' => 'xcvxcvxcv',
+                                'inn' => $testConferenceOrganization->getOrganization()->getInn(),
+                                'kpp' => $testConferenceOrganization->getOrganization()->getKpp(),
+                                'requisites' => 'Тестовое наименование организации ОГРН:',
+                            ),
+                        'notes' => '',
+                        'ConferenceMembers' => [
+                            [
+                                'user' => [
+                                    'lastName' => 'Сюзев',
+                                    'firstName' => 'Евгений',
+                                    'middleName' => '',
+                                    'sex' => '1',
+                                    'phone' => '8(922)209-24-69',
+                                    'email' => 'esuzev+4@test.com',
                                     'post' => 'sdadasd',
                                     'representative' => '1',
                                 ],
