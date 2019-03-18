0c4bcdca5f4bc2a7861e850fb009210aa7a0df29
alf1kknag <e.nachuychenko@nag.ru>
2019-03-18 14:42:40 +0500

    npm i formik && work at program page

---
+++ cms/src/components/Conference/ProgramMemberForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+} from '@material-ui/core';
+import { Formik } from 'formik';
+
+function ProgramMemberForm({ onClose }) {
+
+
+    return (
+        <Dialog>
+            <DialogTitle></DialogTitle>
+        </Dialog>
+    );
+}
+
+export default ProgramMemberForm;
--- cms/src/pages/Program.jsx
+++ cms/src/pages/Program.jsx
@@ -1,25 +1,59 @@
 import React from 'react';
 import {
-    AppBar,
+    Collapse,
+    Paper,
     Tab,
     Tabs,
 } from '@material-ui/core';
+import {
+    RecordVoiceOver,
+    PersonAdd,
+} from '@material-ui/icons';
+import SpeedDialMenu from '../components/utils/SpeedDialMenu';
+
+const actions = [
+    {
+        icon: <RecordVoiceOver/>,
+        title: 'Добавить спикера',
+        tooltipOpen: true,
+        onClick: () => console.log(`Click Add Speaker`),
+    },
+    {
+        icon: <PersonAdd/>,
+        title: 'Добавить члена комитета',
+        tooltipOpen: true,
+        onClick: () => console.log(`Click Add Program Committee Member`),
+    },
+];
 
 function Program() {
-    const [value, setValue] = React.useState(0);
+    const [currentTab, setTab] = React.useState(0);
+    const [showForm, setShowForm] = React.useState(false);
 
-    function handleChange(event, newValue) {
-        setValue(newValue);
+    function handleChangeTab(event, tab) {
+        setTab(tab);
     }
 
     return (
-        <AppBar position={`static`}>
-            <Tabs value={value} variant={`fullWidth`} onChange={handleChange}>
-                <Tab label={`Комитет`}/>
-                <Tab label={`Спикеры`}/>
-                <Tab label={`Расписание`}/>
-            </Tabs>
-        </AppBar>
+        <React.Fragment>
+            <Paper>
+                <Tabs
+                    value={currentTab}
+                    variant={`fullWidth`}
+                    onChange={handleChangeTab}
+                    indicatorColor="primary"
+                    textColor="primary"
+                >
+                    <Tab label={`Комитет`}/>
+                    <Tab label={`Спикеры`}/>
+                    <Tab disabled label={`Расписание`}/>
+                </Tabs>
+            </Paper>
+            <Collapse in={open} unmountOnExit={true}>
+
+            </Collapse>
+            <SpeedDialMenu actions={actions}/>
+        </React.Fragment>
     );
 }
 
