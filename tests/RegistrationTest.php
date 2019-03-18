66f3cd2202a3b02e6dcf998e37833d8880d0d6e4
Evgeniy Syuzev <e.suzev@nag.ru>
2019-03-18 15:38:42 +0500

    тест с созанием организации

--- tests/RegistrationTest.php
+++ tests/RegistrationTest.php
@@ -3,8 +3,14 @@
 namespace App\Tests;
 
 use App\Entity\Participating\ConferenceOrganization;
+use App\Entity\Participating\Organization;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class RegistrationTest
+ * @package App\Tests
+ */
 class RegistrationTest extends WebTestCase
 {
     /**
@@ -15,7 +21,7 @@ class RegistrationTest extends WebTestCase
     /**
      * Проверка усакшности регистрации на открытую конференцию
      */
-    public function testNewRegOk()
+    public function _testNewRegOk()
     {
         $client = static::createClient();
         $crawler = $client->request(
@@ -70,7 +76,7 @@ class RegistrationTest extends WebTestCase
     /**
      * Проверка дубликата пользователя
      */
-    public function testUserExist()
+    public function _testUserExist()
     {
         $client = static::createClient();
         $crawler = $client->request(
@@ -202,11 +208,15 @@ class RegistrationTest extends WebTestCase
 
     /**
      * Проверка что организация найдена по ИНН и КПП и нельзя регистрироваться
+     *
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
      */
     public function testFindOrgByInnFinish()
     {
 
         $kernel = self::bootKernel();
+        /** @var EntityManager $entityManager */
         $entityManager = $kernel->getContainer()
             ->get('doctrine')
             ->getManager();
@@ -271,4 +281,105 @@ class RegistrationTest extends WebTestCase
         $this->assertSame(['errors'=>['inn' => "Организация 'test.organization.name' уже зарегистрирована"]],$error_json);
 
     }
+
+    /**
+     * Проверка обновления организации и доабвления её на конференцию.
+     * Когда организация есть, а заявки на конференцию нет
+     *
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function testFindOrgNoCo()
+    {
+
+        $kernel = self::bootKernel();
+        /** @var EntityManager $entityManager */
+        $entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        /** @var ConferenceOrganization $testConferenceOrganization */
+        $testConferenceOrganization = $entityManager
+            ->getRepository(ConferenceOrganization::class)
+            ->findOneById(self::TEST_CONFERENCE_ORGANIZATION_ID);
+        ;
+
+        $this->assertEquals(self::TEST_CONFERENCE_ORGANIZATION_ID, $testConferenceOrganization->getId() );
+
+        $conference_id = $testConferenceOrganization->getConference()->getId();
+        // Удаляем ConferenceOrganization что бы убедиться в создании новой
+//        ->remove();
+//        foreach($testConferenceOrganization->getConferenceMembers() as $iCM){
+//            //$entityManager->remove($iCM);
+//        }
+//        $entityManager->remove($testConferenceOrganization);
+        $inn = '9999999';
+        $kpp = '99999998';
+        $organization = $entityManager->getRepository( Organization::class )
+            ->findOneBy([
+                'inn' => $inn,
+                'kpp' => $kpp,
+            ]);
+        if (!$organization ) {
+            $organization = new Organization();
+            $organization->setname('test.name')
+                ->setInn('9999999')
+                ->setKpp('99999998');
+        }
+        $entityManager->persist($organization);
+        $entityManager->flush();
+        $inn = $testConferenceOrganization->getOrganization()->getInn();
+        $kpp = $testConferenceOrganization->getOrganization()->getKpp();
+        //if (!);
+
+        $client = static::createClient();
+        $crawler = $client->request(
+            'POST',
+            '/test/registration',
+            [
+                'conference_organization_form' => [
+                    'conference' => $conference_id,
+                    'organization' => [
+                            'name' => 'test.organization.name',
+                            'city' => 'test.organization.city',
+                            'address' => 'test.organization.address',
+                            'inn' => $inn,
+                            'kpp' => $kpp,
+                            'requisites' => 'Тестовое наименование организации ОГРН:',
+                        ],
+                    'notes' => '',
+                    'ConferenceMembers' => [
+                        [
+                            'user' => [
+
+                                'lastName' => 'Сюзев',
+                                'firstName' => 'Евгений',
+                                'middleName' => '',
+                                'sex' => '1',
+                                'phone' => '8(922)209-24-69',
+                                'email' => 'esuzev+4@test.com',
+                                'post' => 'sdadasd',
+                                'representative' => '1',
+                            ],
+                            'arrival' => '23.05.2019 10:00',
+                            'leaving' => '24.05.2019 12:00',
+                            'carNumber' => '',
+                            'RoomType' => '2',
+                            'neighbourhood' => '',
+                        ],
+                    ],
+                    'save' => '',
+                ],
+            ]
+        );
+        $entityManager->remove($organization);
+        $entityManager->flush();
+
+        dump($client->getResponse()->getContent());
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $error_json = json_decode($client->getResponse()->getContent(),true);
+        $this->assertSame(['errors'=>['inn' => "Организация 'test.organization.name' уже зарегистрирована"]],$error_json);
+
+    }
+
 }
