3f23d22799a028e33eafc38c8e624e4ad0f3520c
alf1kknag <e.nachuychenko@nag.ru>
2019-03-26 09:25:13 +0500

    extend WysiwygField props: add height | refactor file upload

--- cms/src/components/utils/WysiwygField.jsx
+++ cms/src/components/utils/WysiwygField.jsx
@@ -4,39 +4,31 @@ import {
     FormControl,
     FormHelperText,
     InputLabel,
-    Input,
 } from "@material-ui/core";
-//import 'ckeditor/ckeditor';
 import CKEditor from 'ckeditor4-react';
 import API from "../../libs/api";
 
 const api = new API();
 
 class WysiwygField extends React.PureComponent {
-    componentDidMount() {
-        const { name } = this.props;
-
-        //CKEDITOR.replace(name, {
-            //filebrowserUploadUrl: `${api.getApiHost()}/attachment/upload_public`,
-            //stylesSet: [],
-        //});
-    }
 
     render() {
-        const { name, label, helperText, onChange, value, error, required, fullWidth } = this.props;
+        const { name, label, helperText, onChange, value, error, required, fullWidth, height } = this.props;
+        let config = {
+            filebrowserUploadUrl: `${api.getApiHost()}/attachment/upload_public`,
+            baseFloatZIndex: 102000,
+        };
+
+        height && (config.height = height);
 
         return (
             <React.Fragment>
                 <FormControl error={error} required={required} fullWidth={fullWidth}>
                     <InputLabel htmlFor={name} required={required} shrink>{label}</InputLabel>
-                    {/*<Input disableUnderline multiline name={name} onChange={onChange} value={value}/>*/}
                     <div style={{ marginTop: 20 }}>
                         <CKEditor
                             data={value}
-                            config={{
-                                filebrowserUploadUrl: `${api.getApiHost()}/attachment/upload_public`,
-                                baseFloatZIndex: 102000,
-                            }}
+                            config={config}
                             onChange={onChange}
                         />
                     </div>
@@ -52,6 +44,7 @@ WysiwygField.propTypes = {
     label:      PropTypes.string.isRequired,
     onChange:   PropTypes.func.isRequired,
 
+    height:     PropTypes.number,
     value:      PropTypes.string,
     helperText: PropTypes.string,
     error:      PropTypes.bool,
--- src/Service/FileManager.php
+++ src/Service/FileManager.php
@@ -3,13 +3,18 @@
 namespace App\Service;
 
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FileManager
 {
     /** @var Filesystem */
     protected $fs;
+
+    /** @var LoggerInterface */
+    protected $logger;
 
     /**
      * Path where file will be upload
@@ -27,13 +32,38 @@ class FileManager
      * FileManager constructor.
      * @param Filesystem $filesystem
      * @param ParameterBagInterface $parameterBag
+     * @param LoggerInterface $logger
      */
-    public function __construct(Filesystem $filesystem, ParameterBagInterface $parameterBag)
+    public function __construct(Filesystem $filesystem, ParameterBagInterface $parameterBag, LoggerInterface $logger)
     {
         $this->fs = $filesystem;
+        $this->logger = $logger;
 
         $this->uploadPath = '/uploads/cros-'.date('Y').'/';
         $this->publicDir  = $parameterBag->get('public_dir');
+    }
+
+    public function uploadFile(UploadedFile $file, $type = null)
+    {
+        $now = new \DateTime();
+
+        $uploadPath = $type
+            ? $this->uploadPath.$type.'/'
+            : $this->uploadPath . $now->format("Y") . '/' . $now->format("m") . '/';
+
+        $filename = $file->getClientOriginalName() . '.'
+            . hash('sha256', $file->getClientOriginalName()) . '.' . $file->guessExtension();
+
+        if (!$this->fs->exists($this->publicDir.$uploadPath)) {
+            $this->fs->mkdir($this->publicDir.$uploadPath);
+        }
+
+        $pathname = $uploadPath.$filename;
+        $savePath = $this->publicDir.$uploadPath;
+
+        $file->move($savePath, $filename);
+
+        return $pathname;
     }
 
     /**
